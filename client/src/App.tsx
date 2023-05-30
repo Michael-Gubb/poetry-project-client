0ed5044ed84cc31b83ad048b2d5f3b26f7b22142
Michael-Gubb <112699394+Michael-Gubb@users.noreply.github.com>
2023-05-30 18:28:47 +1200

    Load poems

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [poems, setPoems] = useState([{ poemText: "placeholder", poemId: 5 }]);
+
+  async function fetchPoems() {
+    const response = await fetch("http://localhost:3333/poems");
+    const data = await response.json();
+    console.log(data);
+
+    setPoems(data.poems);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Poem page</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={fetchPoems}>my button</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {poems.map((poem) => {
+        return (
+          <div key={poem.poemId}>
+            <p>
+              ----------------<br></br>
+            </p>
+            <p>{poem.poemText}</p>
+            <p>
+              ---------------<br></br>
+            </p>
+          </div>
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
