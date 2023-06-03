98769096a96fe2b00c2f18b07a099230bc0140d6
Michael-Gubb <112699394+Michael-Gubb@users.noreply.github.com>
2023-06-03 17:15:14 +1200

    Add topic filter function

--- src/utils/poemUtils.test.tsx
+++ src/utils/poemUtils.test.tsx
@@ -1,5 +1,8 @@
 import { test, expect } from "vitest";
-import { filterPoemByGenreGenerator } from "./poemUtils";
+import {
+  filterPoemByGenreGenerator,
+  filterPoemByTopicGenerator,
+} from "./poemUtils";
 
 test("filterPoemByGenreGenerator returns a function that passes a poem that does not match input", () => {
   const genresToRemove = { poem: true, eulogy: true };
@@ -27,6 +30,38 @@ test("filterPoemByGenreGenerator returns a function that fails a poem that match
     poemId: "1",
     poemDate: "1",
     poemTopics: ["dog"],
+    poemText: "hello",
+    poemImg: "",
+  };
+  expect(filter(inputPoem)).toBe(false);
+});
+
+test("filterPoemByTopicGenerator returns a function that passes a poem that does not match input", () => {
+  const topicsToRemove = { dog: true, fish: true };
+
+  const filter = filterPoemByTopicGenerator(topicsToRemove);
+
+  const inputPoem: Poem = {
+    poemGenre: "poem",
+    poemId: "1",
+    poemDate: "1",
+    poemTopics: ["hello", "goodbye"],
+    poemText: "hello",
+    poemImg: "",
+  };
+  expect(filter(inputPoem)).toBe(true);
+});
+
+test("filterPoemByGenreGenerator returns a function that fails a poem that matches input", () => {
+  const topicsToRemove = { dog: true, fish: true };
+
+  const filter = filterPoemByTopicGenerator(topicsToRemove);
+
+  const inputPoem: Poem = {
+    poemGenre: "poem",
+    poemId: "1",
+    poemDate: "1",
+    poemTopics: ["hello", "fish"],
     poemText: "hello",
     poemImg: "",
   };
--- src/utils/poemUtils.tsx
+++ src/utils/poemUtils.tsx
@@ -7,13 +7,13 @@ interface GenresToRemove {
   [index: string]: boolean;
 }
 
-/** Generates a function to filter poems*/
+/** Generates a function to filter poems by genre*/
 export function filterPoemByGenreGenerator(genresToRemove: GenresToRemove) {
   const genresSet = new Set();
   for (const genre in genresToRemove) {
     genresSet.add(genre);
   }
-  /** Returns false for any peoms with provivded genres  */
+  /** Returns false for any poems with provided genres */
   function filterPoemByGenre(poem: Poem) {
     if (genresSet.has(poem.poemGenre)) {
       return false;
@@ -21,6 +21,28 @@ export function filterPoemByGenreGenerator(genresToRemove: GenresToRemove) {
       return true;
     }
   }
-
   return filterPoemByGenre;
+}
+
+interface TopicsToRemove {
+  [index: string]: boolean;
+}
+/** Generates a function to filter poems by topic*/
+export function filterPoemByTopicGenerator(topicsToRemove: TopicsToRemove) {
+  const topicsSet = new Set();
+  for (const topic in topicsToRemove) {
+    topicsSet.add(topic);
+  }
+  /** Returns false for any poems with provided topics */
+  function filterPoemByTopic(poem: Poem) {
+    for (const topic of poem.poemTopics) {
+      if (topicsSet.has(topic)) {
+        return false;
+      }
+    }
+    {
+      return true;
+    }
+  }
+  return filterPoemByTopic;
 }
